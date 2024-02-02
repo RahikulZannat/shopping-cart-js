d9d258ece1662edd06a1182a3979a465a77fcb0e
Rahikul <rahikuljannat@gmail.com>
2024-02-01 22:08:34 -0500

    remove add for phone

--- javascript/cart.js
+++ javascript/cart.js
@@ -49,3 +49,19 @@ document.getElementById("case-plus").addEventListener("click", function () {
 document.getElementById("case-minus").addEventListener("click", function () {
   updateCaseNumber("case", 59, false);
 });
+
+document.getElementById('phone-clear').addEventListener('click',function(){
+    const phoneNumber = document.getElementById('phone-number');
+    const inputPhone = parseInt(phoneNumber.value);
+    phoneNumber.value = '0';
+
+    const phonePriceTotal = document.getElementById('phone-total');
+    phonePriceTotal.innerText = '0'; 
+
+    const subTotalClear = document.getElementById('sub-total');
+    const subTotalValue = parseInt(subTotalClear.innerText);
+    subTotalClear.innerText = subTotalValue - 1219;
+
+
+
+})
